40385a281ca3691f30330f737bb0120afa07f1f3
Foxtrek_64 <foxtrek64@users.noreply.github.com>
2022-09-14 09:11:52 -0500

    Remove NotFoundError. Add BrowserError.

---
+++ Tafs.Results/Errors/BrowserError.cs
@@ -0,0 +1,20 @@
+﻿//
+//  BrowserError.cs
+//
+//  Author:
+//       TAFS RPA Developers
+//
+//  Copyright (c) 2021 TAFS, LLC.. All rights reserved.
+//
+
+using JetBrains.Annotations;
+using Remora.Results;
+
+namespace Tafs.Results.Errors
+{
+    /// <summary>
+    /// Represents an error which occurred while interfacing with a browser.
+    /// </summary>
+    [PublicAPI]
+    public sealed record class BrowserError(string Message) : ResultError(Message);
+}
